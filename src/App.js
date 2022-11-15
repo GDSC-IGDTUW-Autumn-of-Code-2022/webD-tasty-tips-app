9518eb6b33e7d0f125c5c9263a5a3877f387f98f
Kirti Aggarwal <kirti057btcse21@igdtuw.ac.in>
2022-11-15 08:06:09 -0800

    dark theme

--- src/App.js
+++ src/App.js
@@ -41,9 +41,80 @@ function App() {
     setQuery(search);
   };
 
+  const[mode,setMode]=useState("light");
+  const[myStyle,setStyle]=useState({
+    color:"black"
+  });
+  const[cardStyle,setCard]=useState({
+    display:"inline-block",
+    width:300,
+    marginLeft:100,
+    marginTop:5,
+    marginRight:5
+
+    
+  })
+  /*const[imageStyle,setImage]=useState({
+    height:200
+    
+
+    
+  })*/
+  const toggleMode=()=>{
+    if(mode==="light")
+    {setMode("dark");
+    document.querySelector(".App").style.backgroundImage="radial-gradient(black,black,black)";
+      
+      setStyle({
+        color:"white"
+      })
+      setCard({
+        display:"inline-block",
+        borderWidth:2,
+        borderStyle:"solid",
+        borderColor:"white",
+        width:300,
+        marginLeft:100,
+        marginTop:5,
+        marginRight:5,
+        backgroundColor:"black"
+    
+        
+      })
+     
+      
+    }
+    else{
+      setMode("light");
+    document.querySelector(".App").style.backgroundImage="linear-gradient(rgb(243	229	247),rgb(212	245	239),white)";
+  
+      
+      setStyle(
+        {
+          color:"black"
+        }
+      )
+      setCard({
+        display:"inline-block",
+       /* borderWidth:2,
+        borderStyle:"solid",
+        borderColor:"black",*/
+        width:300,
+        marginLeft:100,
+        marginTop:5,
+        marginRight:5
+    
+        
+      })
+    
+     
+    }
+  }  
+
+
   return (
     <div className="App">
-      <h1 className="heading">Tasty Tips</h1>
+      <h1 className="heading" style={myStyle}>Tasty Tips</h1>
       <form onSubmit={getSearch} className="search-form">
         <input
           className="search-bar"
@@ -54,6 +125,9 @@ function App() {
         <button className="search-btn" type="submit">
           Search
         </button>
+        <div class="form-check form-switch">
+      <input class="form-check-input ms-5" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={toggleMode}></input>
+      </div>
       </form>
       <div className="container">
         {recipes.slice(0, visible).map((r) => (
@@ -64,6 +138,9 @@ function App() {
             img={r.recipe.image}
             url={r.recipe.url}
             ingredients={r.recipe.ingredients}
+            myStyle={myStyle}
+            cardStyle={cardStyle}
+       
           />
         ))}
       </div>
--- src/Recipe.js
+++ src/Recipe.js
@@ -2,18 +2,21 @@ import React from 'react'
 
 export default function Recipe(props) {
     return (
-        <div className="card my-4 mx-3" style={{width: "20rem"}}>
-        <img src={props.img} className="card-img-top"/>
+      <>
+    
+        <div className="card my-4 mx-3" style={props.cardStyle}>
+        <img src={props.img} style={props.imageStyle} className="card-img-top"/>
         <div className="card-body">
-          <h5 className="card-title">{props.title}</h5>
-          <p className="card-text">Calories - {props.calories}</p>
+          <h5 className="card-title" style={props.myStyle}>{props.title}</h5>
+          <p className="card-text" style={props.myStyle}>Calories - {props.calories}</p>
           <ol>
               {props.ingredients.map(i =>(
-                      <li>{i.text}</li>
+                      <li style={props.myStyle}>{i.text}</li>
               ))}
           </ol>
           <a href={props.url} target="_blank" rel="noopener noreferrer" className="btn btn-primary">See the recipe</a>
         </div>
       </div>
+      </>
     )
 }
